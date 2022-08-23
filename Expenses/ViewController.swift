3d87109ec373a342fa21a74246daa309ff4430f7
vlerm <mr.kooper.98@mail.ru>
2022-08-23 03:25:46 +0300

    Initial Commit

---
+++ Expenses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expenses
+//
+//  Created by Вадим Лавор on 23.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
